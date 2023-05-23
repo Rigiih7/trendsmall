8f66c17afdfa2a7fc8c25336289a3881caa03aad
Rigiih7 <mwirigihbrian@gmail.com>
2023-05-23 21:25:48 +0300

    models, class products

--- Rg_ecommerce/rg_trendsmall/models.py
+++ Rg_ecommerce/rg_trendsmall/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.model):
+    image = models.ImageField(upload_to='images/products/large-size')
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
