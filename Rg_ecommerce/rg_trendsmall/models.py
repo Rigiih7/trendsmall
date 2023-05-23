53e8232cd834d6d879693bbf4dceaf41f9ff628f
Rigiih7 <mwirigihbrian@gmail.com>
2023-05-23 23:15:57 +0300

    models and html set up to dynamically fetch data from database

--- Rg_ecommerce/rg_trendsmall/models.py
+++ Rg_ecommerce/rg_trendsmall/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Products(models.model):
     image = models.ImageField(upload_to='images/products/large-size')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=500)
     price = models.IntegerField()
