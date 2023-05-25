5d8dcf6f3d4aa042a69a31d37381f98fb1a65813
Rigiih7 <mwirigihbrian@gmail.com>
2023-05-25 18:58:10 +0300

    database settings in settings files

--- Rg_ecommerce/Rg_ecommerce/settings.py
+++ Rg_ecommerce/Rg_ecommerce/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Rg_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trendsmalldb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
